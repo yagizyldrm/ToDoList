823be7d38f840e1d7c8390d006ed12bdfeb0f9b4
Merve <mervenurkuyumcu@gmail.com>
2021-04-30 00:13:59 +0300

    ToDoNavigations taslak ve theming taslak

--- ToDoList/src/Modules/Navigation/ToDoNavigation.js
+++ ToDoList/src/Modules/Navigation/ToDoNavigation.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Text, TouchableOpacity, View, Button } from 'react-native';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/core';
 import SettingsScreen from "./Screens/SettingsScreen";
+import { colorNames, useThemedColors } from '../Theming';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 const ToDoScreen = props => {
     const navigation = useNavigation();
     return (
-        <View>
-            <TouchableOpacity onPress={() => navigation.navigate('settings-screen')}>
-                <Text>Deneme</Text>
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={{
+                flex:1,
+            }}>
+                <TouchableOpacity onPress={() => navigation.navigate('settings-screen')}>
+                    <Text>Deneme</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end'
+                }}>
+                <Button
+                    title='ekle'
+                    style={{
+                        color:"#000000",
+                        width:20,
+                        height: 20,
+                    }}
+                />
+            </View>
+        </>
     );
 }
 
 const ToDoStack = createStackNavigator();
 
 const ToDoNavigation = () => {
+
+    const colors = useThemedColors();
+
     return (
         <ToDoStack.Navigator>
             <ToDoStack.Screen
                 name="todo-screen"
-                component={ToDoScreen} />
+                component={ToDoScreen}
+                options={{
+                    title: "ToDo List",
+                    headerStyle: {
+                        backgroundColor: '#189ad3',
+                    },
+                    headerTitleAlign: 'center',
+                    headerTitleStyle: {
+                        color: 'white',
+                    },
+                    headerRight: () => (
+                        <Button
+                            onPress={() => alert('This is a button!')}
+                            title="Icon"
+                            color="#000000"
+                        />
+                    ),
+                }}
+            />
             <ToDoStack.Screen
                 name="settings-screen"
                 component={SettingsScreen} />
